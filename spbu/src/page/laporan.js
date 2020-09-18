e9920256ceece1902d40f7c16a6c008470e998b6
ricoramada <42015177+ricoramada@users.noreply.github.com>
2020-09-18 14:34:00 +0700

    react baru

--- spbu/src/page/laporan.js
+++ spbu/src/page/laporan.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import MiniChart from 'react-mini-chart'
+import './laporan.css'
 
 function laporan() {
     return (
-        <div className="laporan">
-            <h1>Laporan</h1>
+        <div>
+            <div className="laporan">
+                {/* Minichart For Live Statik */}
+                <div className="minichart">
+                    <p style={{fontSize: '20px'}}>Statik Data</p>
+                    <MiniChart strokeColor="#FF6600" activePointColor="#FF6600" dataSet={[0, -20, -343, 49.3, 300, 200, 78]}/>
+                </div>
+                <div className="minichart">
+                    <p style={{fontSize: '20px'}}>Masukan Data</p>
+                    <MiniChart dataSet={[0, -20, 100, 49.3, -90, -10, 78]}/>
+                </div>
+                <div className="minichart">
+                    <p style={{fontSize: '20px'}}>Data Live</p>
+                    <MiniChart strokeColor="#FB0000" activePointColor="#FB0000" dataSet={[0, -20, -200, 49.3, -100, 200, 78]}/>
+                </div>
+                <div className="minichart">
+                    <p style={{fontSize: '20px'}}>Data Login</p>
+                    <MiniChart strokeColor="#1B00FB" activePointColor="#1B00FB" dataSet={[0, -20, 343, 49.3, -100, 200, 78]}/>
+                </div>
+
+
+            </div>
         </div>
     )
 }
--- spbu/src/page/user.js
+++ spbu/src/page/user.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react'
 import './user.css'
+import * as ImIcons from 'react-icons/im'
+import * as AiIcons from 'react-icons/ai'
 
 class user extends Component {
     render() {
         return (
             <div className="laman">
                 <div className="card1">
-                    <img src={ require('../gambar/r.JPG') } alt="John" style={{width: '100px'}}/>
+                    <div style={{width: '200px',height: '200px',margin: 'auto',overflow: 'hidden',border: '2px solid black', borderRadius: '50%'}}>
+                        <img src={ require('../gambar/r.JPG') } alt="John" style={{width: '200px'}}/>
+                    </div>
                     <h1>Mukhamad Rico Ramada</h1>
                     <p class="title">Rekayasa Perangkat Lunak</p>
                     <p>SMK Telkom Malang</p>
-                    <a className="k" href="#section"><i class="fa fa-dribbble"></i></a>
-                    <a className="k" href="#section"><i class="fa fa-twitter"></i></a>
-                    <a className="k" href="#section"><i class="fa fa-linkedin"></i></a>
-                    <a className="k" href="#section"><i class="fa fa-facebook"></i></a>
+                     {/* eslint-disable-next-line */}
+                        <a className="a" href="#section" target="_blank"><AiIcons.AiOutlineTwitter/> Twitter</a>
+                        {/* eslint-disable-next-line */}
+                        <a className="a" href="https://www.facebook.com/rico.ramada.31/" target="_blank"><ImIcons.ImFacebook2/> Facebook</a>
+                    <hr/>
                     <p><button className="s">Contact</button></p>
+                </div>
+                <div className="card2">
+                    <h1 style={{marginRight: '120px'}}>Profile</h1>
+                    <p style={{marginRight: '120px'}}>profile bisa diedit</p>
+                    <hr/>
+                    <div className="input">
+                        <div className="inputitem">
+                        <label for="name">Nama</label>
+                        <input type="text" className="form-control" placeholder="Nama" value="Mukhamad Rico Ramada"/>
+                        </div>
+                        <div className="inputitem">
+                        <label for="name">Email</label>
+                        <input type="text" className="form-control" placeholder="Email" value="mukhamad_rico_27rpl@student.smktelkom-mlg.sch.id"/>
+                        </div>
+                        <div className="inputitem">
+                        <label for="name">Jabatan</label>
+                        <input type="text" className="form-control" placeholder="Jabatan" value="Siswa"/>
+                        </div>
+                        <div className="inputitem">
+                        <label for="name">Negara</label>
+                        <input type="text" className="form-control" placeholder="Negara" value="Indonesia"/>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
